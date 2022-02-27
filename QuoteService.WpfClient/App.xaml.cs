972659c470c62e62b6fcdfa33f0214dc2e92f321
Alexander Katkov <afargoth@gmail.com>
2022-02-27 17:59:31 +0700

    Create WpfClient project
    Add a button that connects to the server and gets a qoute
    Show quote in textbox

---
+++ QuoteService.WpfClient/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace QuoteService.WpfClient
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ QuoteService.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuoteService.WpfClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            const int bufferSize = 1024;
+
+            Cursor currentCursor = Cursor;
+            Cursor = Cursors.Wait;
+
+            string serverName = Properties.Settings.Default.ServerName;
+            int port = Properties.Settings.Default.PortNumber;
+
+            TcpClient client = new TcpClient();
+            NetworkStream stream = null;
+
+            try
+            {
+                client.Connect(serverName, port);
+                stream = client.GetStream();
+
+                byte[] buffer = new byte[bufferSize];
+                int received = stream.Read(buffer, 0, bufferSize);
+                if (received <= 0)
+                {
+                    return;
+                }
+
+                TextQuote.Text = Encoding.Unicode.GetString(buffer).Trim('\0');
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, "Quote getting error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+
+                if (client.Connected)
+                {
+                    client.Close();
+                }
+            }
+
+            Cursor = currentCursor;
+        }
+    }
+}
